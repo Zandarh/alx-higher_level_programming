23590368747719bc170fc3d127432f8abca9551b
Zandarh <ubaksxander@gmail.com>
2022-06-02 21:07:03 +0000

    advanced task done

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
         exit(1)
     elif sys.argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
@@ -19,5 +20,5 @@ if __name__ == "__main__":
             print("{:d} {} {:d} = {:d}".format(a, operators[1], b, sub(a, b)))
         elif sys.argv[2] == '*':
             print("{:d} {} {:d} = {:d}".format(a, operators[2], b, mul(a, b)))
-        else:
+        elif sys.argv[2] == '/':
             print("{:d} {} {:d} = {:d}".format(a, opeartors[3], b, div(a, b)))
