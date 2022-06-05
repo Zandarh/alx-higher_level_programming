40c3a13d064201ec6c398c56a39486a6b904d220
Zandarh <ubaksxander@gmail.com>
2022-06-05 05:39:07 +0000

    Hopefully complete

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = 0
+    i = 0
+    if len(my_list) == 0:
+        return (None)
+    else:
+        while i < len(my_list):
+            if max < my_list[i]:
+                max = my_list[i]
+            i += 1
+        return (max)
