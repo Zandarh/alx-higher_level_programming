6497ec87246ae216449ddc25b5aeaa9db5d40be6
Zandarh <ubaksxander@gmail.com>
2022-06-08 16:37:03 +0000

    Mandatory

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def uniq_add(my_list=[]):
     new_set = set(my_list)
     total = 0
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def number_keys(a_dictionary):
     key = len(a_dictionary.keys())
     return key
