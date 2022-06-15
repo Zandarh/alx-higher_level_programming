8af2795b89da7c6af3a790f873f3674c65155aa3
Zandarh <ubaksxander@gmail.com>
2022-06-15 02:24:27 +0000

    Task 5

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
 def raise_exception():
     """
-    raise a type of exception
-    Args: None
-    Return: 
-    
+    raise a type of exception    
     """
-
     raise TypeError
