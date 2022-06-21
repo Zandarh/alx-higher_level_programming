88fdc1ff4ca77d262ba2164f7db832a97cba88b2
Zandarh <ubaksxander@gmail.com>
2022-06-22 00:05:54 +0100

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-    """
-        Class Square defines a square
-        Args:
-            size: Size of a new square.
-    """
+"""
+    Class Square defines a square
+    Args:
+        size: Size of a new square.
+"""
 
-    class Sqaure:
-        def __init__(self, size):
-            self.__size = size
+class Sqaure:
+    def __init__(self, size):
+        self.__size = size
