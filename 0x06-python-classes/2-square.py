f8ae547b3631cc9e2957b3bfec20e06b5fe92070
Zandarh <ubaksxander@gmail.com>
2022-06-22 15:46:37 +0100

    Task update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """define a class square."""
 
 
@@ -6,13 +7,11 @@ class Square:
     """Declares a square class"""
 
     def __init__(self, size=0):
-        """
-        initialize  class attribute.
+        """initialize  class attribute.
 
         Args:
             size (int): The size of a new square.
         """
-
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
