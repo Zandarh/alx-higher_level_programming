9422bda9a918cabdb21de5bc173d74363a8ebe9f
Zandarh <ubaksxander@gmail.com>
2022-06-28 01:42:19 +0100

    Task update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,25 +7,39 @@ class Rectangle:
     """ Represent a square """
 
     def __init__(self, height=0, width=0):
+        """
+            Initialization of the Rectangle class
+
+            Args:
+                width: width of the rectangle
+                height: height of the rectangle
+        """
         self.__height = height
         self.__width = width
+
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
-        if isinstance(value, int) == False:
+        if isinstance(value, int) is False:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
     @property
     def height(self):
+        """
+            Gets the private instance height used by the class
+        """
         return self.__height
+
     @height.setter
     def height(self, value):
-        if isinstance(value, int) == False:
+        if isinstance(value, int) is False:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -7,34 +7,59 @@ class Rectangle:
     """ Defines a rectangle with an area"""
 
     def __init__(self, width=0, height=0):
+        """
+            Initialization of the Rectangle class
+
+            Args:
+                width: width of the rectangle
+                height: height of the rectangle
+        """
         self.__width = width
         self.__height = height
+
     @property
     def width(self):
+        """
+            Gets the private instance width used by the class
+        """
         return self.__width
+
     @width.setter
     def width(self, value):
-        if isinstance(value, int) == False:
+        if isinstance(value, int) is False:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
     @property
     def height(self):
+        """
+            Gets the private instance height used by the class
+        """
         return self.__height
+
     @height.setter
     def height(self, value):
-        if isinstance(value, int) == False:
+        if isinstance(value, int) is False:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
     def area(self):
+        """
+            Calculates the area of the rectangle
+        """
         return (self.__width * self.__height)
+
     def perimeter(self):
+        """
+            Calculates the perimeter of the rectangle
+        """
         if self.__width == 0 or self.__height == 0:
             return (0)
         else:
-            return (2 *(self.__width + self.__height))
+            return (2 * (self.__width + self.__height))
