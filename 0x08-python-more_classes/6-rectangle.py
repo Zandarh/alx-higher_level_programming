c92729f172cff073b8347033ff9133a91b0d04d9
Zandarh <ubaksxander@gmail.com>
2022-06-28 02:38:25 +0100

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@
 class Rectangle:
     """ This is a rectangle class """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """
             Initialization of the Rectangle class
