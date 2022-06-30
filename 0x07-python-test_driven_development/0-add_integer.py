f3d13ab900bdf40f27b95fb4d1796770f5f626ba
Zandarh <ubaksxander@gmail.com>
2022-06-30 10:18:00 +0100

    Test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,12 +13,18 @@ def add_integer(a, b=98):
 
     if isinstance(a, int) is False:
         if isinstance(a, float) is True:
-            a = int(a)
+            if a != a: #checking for NaN numbers
+                raise TypeError("a must be an integer")
+            else:
+                a = int(a) #typcasting float to int
         else:
             raise TypeError("a must be an integer")
     if isinstance(b, int) is False:
         if isinstance(b, float) is True:
-            b = int(b)
+            if b != b: #checking for NaN numbers
+                raise TypeError("b must be an integer")
+            else:
+                b = int(b) #typcasting float to int
         else:
             raise TypeError("b must be an integer")
     return (a + b)
