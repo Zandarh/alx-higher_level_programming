ff2a623704088d263a0e87d1f614238c6bca9511
Zandarh <ubaksxander@gmail.com>
2022-07-05 02:45:46 +0100

    Task 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    1-list module
+"""
+
+
+class MyList (list):
+    """
+        subclass of list
+    """
+
+    def print_sorted(self):
+        """
+            Sorts the list before printing
+        """
+        print(sorted(self))
