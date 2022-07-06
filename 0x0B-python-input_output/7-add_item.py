ce83f2a6f718321ec75bd97f56ee4dc409ba4100
Zandarh <ubaksxander@gmail.com>
2022-07-06 14:47:44 +0100

    Tsk 8

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    7-add_item module
+"""
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = 'add_item.json'
+my_list = []
+
+try:
+    my_list = load_from_json_file(filename)
+except Exception:
+    save_to_json_file(my_list, filename)
+
+arg_len = len(argv)
+
+if arg_len > 1:
+    for i in range(i, arg_len):
+        my_list.append(argv[i])
+    save_to_json_file(my_list, filename)
