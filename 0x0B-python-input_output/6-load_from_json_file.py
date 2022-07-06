e0b50c9c0901f624ce32a5e7fcf1f8db5e165134
Zandarh <ubaksxander@gmail.com>
2022-07-06 14:35:14 +0100

    task 7

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    6-load_from_json_file module
+"""
+import json as js
+
+
+def load_from_json_file(filename):
+    """
+        function that creates an Object from a “JSON file”
+
+        Args:
+            filename: The name of the file
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return js.load(file)
