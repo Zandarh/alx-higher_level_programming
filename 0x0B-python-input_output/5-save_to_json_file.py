bcdff37bc0667d35f3bf002426fd11fdc3c282b5
Zandarh <ubaksxander@gmail.com>
2022-07-06 14:30:12 +0100

    task 4

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,7 +8,7 @@ import json as js
 def save_to_json_file(my_obj, filename):
     """
         writes an Object to a text file, using a JSON representation
-    
+
         Args:
             my_obj: Th object
             filename: the text file
