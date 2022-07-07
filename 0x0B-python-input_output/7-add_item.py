6cdbe544ec04d72f8dff66d0444ce21d0c47c7ec
Zandarh <ubaksxander@gmail.com>
2022-07-07 11:58:28 +0100

    Task update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,11 @@
     7-add_item Module
 """
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-
 if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
     filename = 'add_item.json'
     my_list = []
     try:
