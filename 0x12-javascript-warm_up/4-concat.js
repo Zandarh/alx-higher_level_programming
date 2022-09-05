f100de7d9c9419a55fcc01f6aff1e59404aefaac
Zandarh <ubaksxander@gmail.com>
2022-09-05 20:27:38 +0100

    JS

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+    console.log('undefined is undefined')
+} else if (process.argv.lenght === 4) {
+    console.log(`${process.argv[2]} is undefined`);
+} else {
+    console.log(`${process.argv[2]} is ${process.argv[3]}`)
+}
