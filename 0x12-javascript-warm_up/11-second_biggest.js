5118f731b81959e779d344445cf5d50c2dd86780
Zandarh <ubaksxander@gmail.com>
2022-09-06 01:43:23 +0100

    Bug fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv;
-  list.splice(null, 2);
-  list.sort();
-  list.pop();
-  const max = Math.max(...list);
-  console.log(max);
+  const list = process.argv.map(Number)
+  .slice(2, process.argv.length)
+  .sort((a, b) => a -b);
+  console.log(list[list.length - 2]);
 }
