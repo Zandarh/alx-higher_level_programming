c7e5cb47192de6982a438c3adc4b3ec97e6b1531
Zandarh <ubaksxander@gmail.com>
2022-09-15 09:29:56 +0100

    taskses

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,12 +6,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    if (sys.argv) < 5:
-        print("Usage: ./<script_name>.py [username]"
-              "[password] [database] [state]")
-        sys.exit()
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `cities` as `c` \
