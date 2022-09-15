97aeb1a75b6899a72548d40cf742c419d7a4bcae
Zandarh <ubaksxander@gmail.com>
2022-09-15 11:28:52 +0100

    taskses

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""adds the State object """
+"""adds the State object"""
 
 import sys
 from model_state import Base, State
@@ -13,9 +13,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name ='Louisiana')
+    new_state = State(name='Louisiana')
     session.add(new_state)
     new_instance = session.query(State).filter_by(name='Louisiana').first()
     print(new_instance.id)
     session.commit()
-    
