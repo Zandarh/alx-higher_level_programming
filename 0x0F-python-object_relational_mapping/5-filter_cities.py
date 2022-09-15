f93d517513e26da3c2afae25d68eb5bc7093f0a3
Zandarh <ubaksxander@gmail.com>
2022-09-15 09:28:46 +0100

    taskses

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    print(", ".join([ct[2] for ct in cur.fetchall() if ct[4] == sys.argv[4]]))
     cur.close()
     db.close()
