a6d6a0b82c86059684b7d3d2c19f7e31ed3752f2
Zandarh <ubaksxander@gmail.com>
2022-10-28 22:32:19 +0100

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 const fs = require('fs');
 
 const file = process.argv[2];
-fs.readFile(file, 'utf-8;, (error, data) => {
+fs.readFile(file, 'utf-8', (error, data) => {
 	if (error) {
 		console.log(error);
 	}
-	console.log(data):
+	console.log(data);
 });
